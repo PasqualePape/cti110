fa534b24887ef696d7f16bd2d47fb0a23e597b11
PasqualePape <papep3348@student.faytechcc.edu>
2024-09-16 11:19:25 -0400

    Add files via upload
    First submission

---
+++ P1Lab1_PapePasquale.py
@@ -0,0 +1,13 @@
+# Pasquale Pape
+# 9/16/2024
+# P1Lab1
+# get input from the user and display output.
+
+#get first name from user
+first_name = input("Enter your first name:" )
+    
+#get last name from user
+last_name = input("Enter your last name:" )
+
+#display info to user
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
